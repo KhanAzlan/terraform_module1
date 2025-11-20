d61bc13ba1751337d208ebd65bb5828f45c9885d
KhanAzlan <azlankhan7266@gmail.com>
2025-11-20 23:44:27 +0530

    Update main.tf

--- day1/main.tf
+++ day1/main.tf
@@ -1,1 +1,9 @@
+provider "aws" {
+  region = "eu-north-1"
+}
 
+module "module" {
+  source = "./modules"
+  ami_value = "ami-0fa91bc90632c73c9" # replace this
+  instance_type_value = "t3.micro" # replace this
+}
